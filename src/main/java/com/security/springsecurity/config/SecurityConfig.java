eaf74046250639c5c8ca2a630165f002c99c982b
adsuman333 <adarshsuman003@gmail.com>
2025-04-23 00:03:41 +0530

    Spring security user from database
    Creating hard-coded usernames and passwords in our file

--- src/main/java/com/security/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/security/springsecurity/config/SecurityConfig.java
@@ -7,6 +7,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -46,13 +50,35 @@ public class SecurityConfig {
 //        httpSecurity.authorizeHttpRequests()
 
         //Builder pattern
-        httpSecurity
+        return httpSecurity
                 .csrf(customizer -> customizer.disable())
                 .authorizeHttpRequests(request -> request.anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        return httpSecurity.build();
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails user1 = User
+                .withDefaultPasswordEncoder()
+                .username("Kiran")
+                .password("nariK")
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User
+                .withDefaultPasswordEncoder()
+                .username("Chanda")
+                .password("adnahC")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+
+    }
+
 }
